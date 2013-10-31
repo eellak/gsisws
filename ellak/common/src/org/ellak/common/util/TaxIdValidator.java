124057dd1e6163f31a03cb09de1c9af7e064aa0e
vassterg <vassterg@gmail.com>
2013-10-31 21:09:04 +0200

    changed taxId validator to something more efficient

--- ellak/common/src/org/ellak/common/util/TaxIdValidator.java
+++ ellak/common/src/org/ellak/common/util/TaxIdValidator.java
@@ -4,44 +4,30 @@ package org.ellak.common.util;
 /**
  * @author George Lamprakis
  * @date Oct 28, 2013
+ *
+ * @author Vasilis Stergioulis
+ * @date Oct 31, 2013
+ * @version  1.0 more efficient validator
+ * 
  */
 public class TaxIdValidator {
 	//---------------------------------------------------------------------------------------
     public static boolean checkTaxId(String taxId) {
-        int nExp = 1;
-        try {
-            Integer.parseInt(taxId);
-            taxId = taxId.trim();
-            if (taxId.length() != 9){
-                return false;
-            }
-            int nSum = 0;
-            int xDigit = 0;
-            int nT = 0;
-            for (int i = taxId.length() - 2; i >=0; i--) {
-                xDigit = Integer.parseInt((taxId.substring (i, i+1)));
-                nT = xDigit * (int) (Math.pow(2,nExp));
-                nSum += nT;
-                nExp++;
-            }
-            xDigit = Integer.parseInt(taxId.substring (taxId.length()-1, taxId.length()));
-            nT = nSum / 11;
-            int k = nT * 11;
-            k = nSum - k;
-            if (k == 10) {
-                k = 0;
-            }
-            if (xDigit != k) {
-                return false;
-            }
-            return true;
-        } catch (NumberFormatException e) {
-        	e.printStackTrace();
-            return false;
-        } catch (Exception e) {
-        	e.printStackTrace();
-            return false;
-        }
+	        try {
+	            Integer.parseInt(taxId);
+	        } catch (NumberFormatException e) {
+	            return false;
+	        }
+	        
+	        if (taxId.trim().length() != 9) {
+	            return false;
+	        }
+
+	        int sum = 0;
+	        for (int i = 0; i < 8; i++) {
+	            sum += (taxId.charAt(i) - '0') << (8 - i);
+	        }
+	        return ((taxId.charAt(8) - '0') == ((sum % 11) % 10));
     }   
     //---------------------------------------------------------------------------------------
 }
